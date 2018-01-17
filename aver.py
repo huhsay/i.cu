a193548cf01ce07d998a68c60be3b306b5ea1f6c
theo <bethejustice@naver.com>
2018-01-17 19:56:55 +0900

    fix

--- aver.py
+++ aver.py
@@ -27,29 +27,24 @@ def term():
 	sql = "SELECT LAST_INSERT_ID(id)  FROM raw"
 
 	num=curs.execute(sql)
-	curs.execute("select time FROM raw where id=1")
-	rows=curs.fetchall()
-	date=rows[0][0].date()
-	stime=rows[0][0].time()
+	if(num != 1):
+		curs.execute("select time FROM raw where id=1")
+		rows=curs.fetchall()
+		date=rows[0][0].date()
+		stime=rows[0][0].time()
 
-	curs.execute("select time FROM raw where id=%s",(num) )
-	rows=curs.fetchall()
-	etime=rows[0][0].time()
+		curs.execute("select time FROM raw where id=%s",(num) )
+		rows=curs.fetchall()
+		etime=rows[0][0].time()
 
-	curs.execute("SELECT ROUND(AVG(leftdata),1) FROM raw")
-	rows=curs.fetchone()
-	laver=rows[0]
-	curs.execute("SELECT ROUND(AVG(rightdata),1) FROM raw")
-	rows=curs.fetchone()
-	raver=rows[0]
+		curs.execute("SELECT ROUND(AVG(leftdata),1) FROM raw")
+		rows=curs.fetchone()
+		laver=rows[0]
+		curs.execute("SELECT ROUND(AVG(rightdata),1) FROM raw")
+		rows=curs.fetchone()
+		raver=rows[0]
 
-	print laver
-	print raver
+		curs.execute("INSERT INTO term(date, stime, etime, laver, raver) values (%s, %s, %s, %s, %s)", (date, stime, etime, laver, raver))
+		conn.commit()
 
-	curs.execute("INSERT INTO term(date, stime, etime, laver, raver) values (%s, %s, %s, %s, %s)", (date, stime, etime, laver, raver))
-	conn.commit()
 	curs.execute("truncate raw") #init table
-
-#atomgit
-#atomgit2
-#atomgit3
